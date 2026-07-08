fd838c0b664c1cdbcfeaeb43f495f561869e2a2b
AyushPrad2907 <ayushpradhan2949@gmail.com>
2026-07-08 23:33:07 +0530

    Update BookCard to use new book cover image (.jpeg)

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -3,7 +3,7 @@ import { getBackendAssetUrl } from '../services/api';
 import './BookCard.css';
 
 const BookCard = ({ book }) => {
-  const coverImage = book.coverImage ? getBackendAssetUrl(book.coverImage) : '/book-cover.jpg';
+  const coverImage = book.coverImage ? getBackendAssetUrl(book.coverImage) : '/book-cover.jpeg';
 
   return (
     <div className="book-card card fade-in reveal-card" data-reveal>
